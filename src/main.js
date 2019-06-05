a845248753eb1861aa0b86ee9e8e0ff8dfd3cccd
brian hensley and marc davies <brian@>
2019-06-05 14:09:04 -0700

    add functionality to generate a word through API call

--- src/main.js
+++ src/main.js
@@ -5,6 +5,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-  let className = new ClassName;
-  return className;
+  $('#wordGenerator').click(function(event) {
+    event.preventDefault();
+    let words = $('#words').val();
+    $('#words').val("");
+    $.ajax({
+      url: `http://dinoipsum.herokuapp.com/api/?paragraphs=1&words=1`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        $('#wordGuess').text(`${response}`);
+        console.log(response);
+      },
+      error: function() {
+        $('#errors').text("There was a problem generating your word. Try again.");
+      }
+    });
+  });
 });
